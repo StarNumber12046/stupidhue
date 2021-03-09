b5e2380c85696899c642afbfa32211de5d468a72
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2021-03-09 15:49:46 +0100

    Rename main.py to main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+from phue import Bridge
+from time import sleep
+ip = ''
+b = Bridge(ip)
+b.connect()
+# Get the bridge state (This returns the full dictionary that you can explore)
+b.get_api()
+
+b.get_light(1, 'on')
+# Prints if light 1 is on or not
+b.get_light('Hue iris di Francesco e Filippo')
+lights = b.lights
+# Print light names
+for l in lights:
+    print(l.name)
+
+# Set brightness of each light to 127
+for l in lights:
+    sas = b.get_light(1, 'on')
+    print(sas)
+
+    b.set_light(1, 'on', True)
+    l.brightness = 127
+start = True
+color = 0
+
+b.set_light(1, 'on', True)
+while start:
+    while color < 65400:
+        color = color + 200
+        b.set_light(1, 'hue', color)
+        print(color)
+    if color == 200:
+        pass
+    else:
+        color = 1
+        b.set_light(1, 'hue', color)
+        print(color)
